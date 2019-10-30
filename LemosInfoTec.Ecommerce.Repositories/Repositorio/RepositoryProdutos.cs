81a1cd735c2698c0b377423362e7d53158fb3b30
edisantos <edinaldolemos@yahoo.com.br>
2019-10-30 19:56:47 -0300

    Repositorios

--- LemosInfoTec.Ecommerce.Repositories/Repositorio/RepositoryProdutos.cs
+++ LemosInfoTec.Ecommerce.Repositories/Repositorio/RepositoryProdutos.cs
@@ -1,12 +1,13 @@
+using System;
 using LemosInfotec.Ecommerce.Domain.Contratos;
 using LemosInfotec.Ecommerce.Domain.Entidades;
 using LemosInfoTec.Ecommerce.Repositories.Repositorio.Generics;
 
 namespace LemosInfoTec.Ecommerce.Repositories.Repositorio
 {
-    public class RepositoryUsuario:RepositoryBase<Usuarios>,IRepositoryUsuario
+    public class RepositoryProdutos:RepositoryBase<Produtos>,IRepositoryProdutos
     {
-        public RepositoryUsuario()
+        public RepositoryProdutos()
         {
             
         }
