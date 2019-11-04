37405e794b9d0ff92a1ee4ed76d1fe09ee3d74b3
edisantos <edinaldolemos@yahoo.com.br>
2019-11-04 20:15:35 -0300

    Contexto

--- LemosInfoTec.Ecommerce.Repositories/DataContexto/DbContexto.cs
+++ LemosInfoTec.Ecommerce.Repositories/DataContexto/DbContexto.cs
@@ -1,16 +1,21 @@
 using System;
+using JetBrains.Annotations;
+using LemosInfotec.Ecommerce.Domain.Entidades;
+using LemosInfotec.Ecommerce.Domain.ObjetosValores;
 using Microsoft.EntityFrameworkCore;
 
 namespace LemosInfoTec.Ecommerce.Repositories.DataContexto
 {
     public class DbContexto:DbContext
     {
-        public DbContexto(DbContextOptions<DbContexto> options)
-        :base(options)
+        public DbContexto(DbContextOptions options) : base(options)
         {
-            
         }
 
-        public DbSet<usuarios> Usuarios{get;set;}
+        public DbSet<Usuarios> Usuarios{get;set;}
+        public DbSet<Pedidos> Pedidos{get;set;}
+        public DbSet<ItensPedidos> ItensPedidos{get;set;}
+        public DbSet<Produtos> Produtos{get;set;}
+        public DbSet<FormaPagamento> FormaPagamentos{get;set;}
     }
 }
